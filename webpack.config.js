141e3896d76d5de4602faf2962a5140de4df13b3
Nikola <nikola88393@gmail.com>
2024-01-08 16:30:48 +0200

    Create intial setup of the project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/template.html'
+        })
+    ]
+}
